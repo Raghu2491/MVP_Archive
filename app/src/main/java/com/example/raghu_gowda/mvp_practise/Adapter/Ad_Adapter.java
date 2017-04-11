fd53df15f387e5e624be0be0fddeeeb7f6273938
Raghu2491 <raghavendra0204@gmail.com>
2017-04-11 03:55:51 -0500

    Added Recycler View, Adapter for loading ads and listener to listen for card click

---
+++ app/src/main/java/com/example/raghu_gowda/mvp_practise/Adapter/Ad_Adapter.java
@@ -0,0 +1,78 @@
+package com.example.raghu_gowda.mvp_practise.Adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.raghu_gowda.mvp_practise.Model.Ad;
+import com.example.raghu_gowda.mvp_practise.Model.Ads;
+import com.example.raghu_gowda.mvp_practise.R;
+import com.example.raghu_gowda.mvp_practise.UI.MainActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Admin on 4/10/17.
+ */
+
+public class Ad_Adapter extends RecyclerView.Adapter<Ad_Adapter.ViewHolder> {
+
+    private final LayoutInflater inflator;
+    private List<Ad> ads;
+    OnEachcardSelected mlistener;
+
+
+    public Ad_Adapter(OnEachcardSelected listener,LayoutInflater inflater){
+        this.inflator=inflater;
+        this.ads=new ArrayList<>();
+        mlistener=listener;
+    }
+
+    @Override
+    public Ad_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new Ad_Adapter.ViewHolder(inflator.inflate(R.layout.item_ad,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(final Ad_Adapter.ViewHolder holder, int position) {
+        Ad _ad=ads.get(position);
+        holder.mText.setText(_ad.getProductName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return ads.size();
+    }
+
+    public void newAds(Ads mAds) {
+        //ads= mAds.getAd();
+        ads.addAll(mAds.getAd());
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        private TextView mText;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mText=(TextView)itemView.findViewById(R.id.description);
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            mlistener.onCardClicked(getLayoutPosition(),ads.get(getAdapterPosition()).getCategoryName());
+        }
+
+
+    }
+
+    public interface OnEachcardSelected{
+        void onCardClicked(int position,String name);
+    }
+}
--- app/src/main/java/com/example/raghu_gowda/mvp_practise/UI/MainActivity.java
+++ app/src/main/java/com/example/raghu_gowda/mvp_practise/UI/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.raghu_gowda.mvp_practise.UI;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.raghu_gowda.mvp_practise.Adapter.Ad_Adapter;
 import com.example.raghu_gowda.mvp_practise.Model.Ads;
 import com.example.raghu_gowda.mvp_practise.Model.Category;
 import com.example.raghu_gowda.mvp_practise.Model.MyPojo;
@@ -18,10 +26,12 @@ import com.example.raghu_gowda.mvp_practise.base.CategoryPresenter;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import retrofit2.http.Query;
 import rx.Observable;
 
-public class MainActivity extends AppCompatActivity implements CategoryViewInterface,AdsViewInterface {
+public class MainActivity extends AppCompatActivity implements CategoryViewInterface,AdsViewInterface, Ad_Adapter.OnEachcardSelected {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     String id="236";
@@ -30,26 +40,44 @@ public class MainActivity extends AppCompatActivity implements CategoryViewInter
     String deviceId="4230";
     String sessionId="techtestsession";
     String totalCampaignsRequested="10";
+    private AdPresenter adPresenter;
+    private Ad_Adapter mAdapter;
+    private ProgressDialog mDialog;
 
 
     @Inject
     AdService adService;
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
 
-    //private CategoryPresenter categoryPresenter;
-    private AdPresenter adPresenter;
 
-    public static final String API_KEY="2f43365b380316d3a234157ae5e31";
-    public String group_urlname="ny-tech";
+//    private CategoryPresenter categoryPresenter;
+//    public static final String API_KEY="2f43365b380316d3a234157ae5e31";
+//    public String group_urlname="ny-tech";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         resolveDependencies();
+        configViews();
 
         adPresenter=new AdPresenter(MainActivity.this);
         adPresenter.onCreate();
 
+
+    }
+
+    private void configViews() {
+        recyclerView.setRecycledViewPool(new RecyclerView.RecycledViewPool());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter=new Ad_Adapter(this,getLayoutInflater());
+        recyclerView.setAdapter(mAdapter);
 
     }
 
@@ -66,21 +94,28 @@ public class MainActivity extends AppCompatActivity implements CategoryViewInter
 //        categoryPresenter.fetchCategories();
         adPresenter.onResume();
         adPresenter.fetchAds();
+        mDialog=new ProgressDialog(MainActivity.this);
+        mDialog.setIndeterminate(true);
+        mDialog.setProgress(ProgressDialog.STYLE_SPINNER);
+        mDialog.setTitle("Downloading");
+        mDialog.setMessage("Please Wait...");
+        mDialog.show();
     }
 
     @Override
     public void completed() {
-
+        mDialog.dismiss();
     }
 
     @Override
     public void onError(String message) {
-        Log.e(TAG,message);
+        mDialog.dismiss();
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
     }
 
     @Override
-    public void onAds(Ads mypojo) {
-        System.out.println("------->"+mypojo);
+    public void onAds(Ads mAds) {
+        mAdapter.newAds(mAds);
     }
 
 
@@ -105,4 +140,9 @@ public class MainActivity extends AppCompatActivity implements CategoryViewInter
                 );
     }
 
+    @Override
+    public void onCardClicked(int position,String name) {
+        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+
+    }
 }
